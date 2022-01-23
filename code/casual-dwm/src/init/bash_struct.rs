12bca92313f9d8d97462c8f63ed9f0ce88a4cde3
Markus <contact@zgast.at>
2022-01-23 01:24:57 +0100

    brought in some ideas, not compileable

--- code/casual-dwm/src/init/bash_struct.rs
+++ code/casual-dwm/src/init/bash_struct.rs
@@ -3,5 +3,6 @@ pub fn bash_struct() -> HashMap<String, String> {
     let mut map = HashMap::new();
 
     map.insert(String::from("composing"),String::from("picom -f &"));
+    map.insert(String::from("background"),String::from("feh --bg-scale "));
     return map;
 }
--- code/casual-dwm/src/init/init_check.rs
+++ code/casual-dwm/src/init/init_check.rs
@@ -5,20 +5,29 @@ use std::fs;
 pub fn check_config(config_vec: Vec<Yaml>){
     let config = &config_vec[0];
     let initrc_file = fs::read_to_string("/home/markus/Documents/GitHub/casual-dwm/dwm/.initrc").unwrap();
-    let features = bash_struct();
+    let mut features = bash_struct();
+    let mut initrc: Vec<&str> = initrc_file.split("\n").clone().collect();
 
-    let mut output = initrc_file.clone();
-    if config["base"]["composing"].as_bool().unwrap() {
-        let composing_feature = features.get("composing").unwrap();
-        let mut composing_bool = false;
-        for line in initrc_file.lines() {
-            if !line.eq(features.get("composing").unwrap()) && !composing_bool{
-                output.push('\n');
-                output.push_str(composing_feature);
-                composing_bool = true;
-            }
+    let composing_feature = features.get("composing").unwrap();
+    let mut composing_bool = false;
+    let composing_activated = config["base"]["composing"].as_bool().unwrap();
+
+    let background_path = config["base"]["background"].as_str().unwrap();
+
+    for line in initrc {
+        if  composing_activated && !line.eq(composing_feature) && !composing_bool{
+            output.push('\n');
+            output.push_str(composing_feature);
+            composing_bool = true;
+        } else if !composing_activated && line.eq(composing_feature){
+            line = "";
+        }
+
+        if !line.contains(background_path){
+
         }
     }
+
 
     println!("{}", output);
 
