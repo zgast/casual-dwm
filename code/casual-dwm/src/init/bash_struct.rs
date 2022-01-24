0c70a37e219d7f70faa1b167d939058a7b580b9a
Markus <contact@zgast.at>
2022-01-24 12:00:06 +0100

    added auto_off: off

--- code/casual-dwm/src/init/bash_struct.rs
+++ code/casual-dwm/src/init/bash_struct.rs
@@ -10,6 +10,7 @@ pub fn bash_struct() -> HashMap<String, String> {
     map.insert(String::from("auto_off_fast"),String::from("xset dpms 20 80 100"));
     map.insert(String::from("auto_off_mid"),String::from("xset dpms 60 120 180"));
     map.insert(String::from("auto_off_slow"),String::from("xset dpms 100 200 260"));
+    map.insert(String::from("auto_off_off"),String::from(" # xset dpms 100 200 260"));
     map.insert(String::from("auto_off_generic"),String::from("xset dpms"));
     return map;
 }
